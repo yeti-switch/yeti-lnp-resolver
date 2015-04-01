e1888ed26c0de2d7b51cda8a60598fcb72bdf5e7
murfur <void@onat.edu.ua>
2015-04-01 16:48:10 +0300

    log: reduce useless in messages

--- server/src/log.h
+++ server/src/log.h
@@ -16,14 +16,15 @@ extern int log_level;
 #define cerr(fmt,args...) fprintf(stderr,"error: "fmt"\n",##args);
 
 #ifdef VERBOSE_LOGGING
-#define _LOG(level,fmt,args...) syslog(level,"[%u] %s:%d:%s "fmt,syscall(__NR_gettid), __FILENAME__,__LINE__,__PRETTY_FUNCTION__,##args);
+//#define _LOG(level,fmt,args...) syslog(level,"[%u] %s:%d:%s "fmt,syscall(__NR_gettid), __FILENAME__,__LINE__,__PRETTY_FUNCTION__,##args);
+#define _LOG(level,level_str,fmt,args...) syslog(level,"[%u] "level_str"%s:%d: "fmt,syscall(__NR_gettid), __FILENAME__,__LINE__,##args);
 #else
-#define _LOG(level,fmt,args...) syslog(level,fmt,##args);
+#define _LOG(level,level_str,fmt,args...) syslog(level,fmt,##args);
 #endif
 
-#define err(fmt,args...) _LOG(LOG_ERR,"error: "fmt,##args);
-#define info(fmt,args...) if(log_level > L_ERR) _LOG(LOG_INFO,"info: "fmt,##args);
-#define dbg(fmt,args...) if(log_level > L_INFO) _LOG(LOG_DEBUG,"dbg: "fmt,##args);
+#define err(fmt,args...) _LOG(LOG_ERR,"error: ",fmt,##args);
+#define info(fmt,args...) if(log_level > L_ERR) _LOG(LOG_INFO,"info: ",fmt,##args);
+#define dbg(fmt,args...) if(log_level > L_INFO) _LOG(LOG_DEBUG,"dbg: ",fmt,##args);
 
 void open_log();
 void close_log();
