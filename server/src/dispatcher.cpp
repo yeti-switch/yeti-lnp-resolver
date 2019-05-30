3d91b08c13a92a63ac32decf952b2540c92e2753
Michael Furmur <m.furmur@gmail.com>
2019-05-31 01:46:35 +0300

    dispatcher: zero-copy reply buffer. fix identation

--- server/src/dispatcher.cpp
+++ server/src/dispatcher.cpp
@@ -15,165 +15,168 @@ _dispatcher::_dispatcher()
 
 void _dispatcher::loop()
 {
-	int ret;
-	int epoll_fd;
-	int nn_poll_fd;
+    int ret;
+    int epoll_fd;
+    int nn_poll_fd;
 
-	struct epoll_event ev;
-	struct epoll_event events[EPOLL_MAX_EVENTS];
+    struct epoll_event ev;
+    struct epoll_event events[EPOLL_MAX_EVENTS];
 
-	if(-1 == (stop_event_fd = eventfd(0, EFD_NONBLOCK)))
-		throw std::string("eventfd call failed");
+    if(-1 == (stop_event_fd = eventfd(0, EFD_NONBLOCK)))
+        throw std::string("eventfd call failed");
 
-	if((epoll_fd = epoll_create(EPOLL_MAX_EVENTS)) == -1)
-		throw std::string("epoll_create call failed");
+    if((epoll_fd = epoll_create(EPOLL_MAX_EVENTS)) == -1)
+        throw std::string("epoll_create call failed");
 
-	ev.events = EPOLLIN;
+    ev.events = EPOLLIN;
 
-	ev.data.fd = stop_event_fd;
-	if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, stop_event_fd, &ev) == -1)
-		throw std::string("epoll_ctl call for stop_event_fd failed");
+    ev.data.fd = stop_event_fd;
+    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, stop_event_fd, &ev) == -1)
+        throw std::string("epoll_ctl call for stop_event_fd failed");
 
-	s = nn_socket(AF_SP,NN_REP);
-	if(s < 0){
-		throw std::string("nn_socket()");
-	}
+    s = nn_socket(AF_SP,NN_REP);
+    if(s < 0){
+        throw std::string("nn_socket()");
+    }
 
-	size_t vallen;
-	if(0!=nn_getsockopt(s, NN_SOL_SOCKET, NN_RCVFD, &nn_poll_fd, &vallen)) {
-		throw std::string("nn_setsockopt(NN_LINGER)");
-	}
+    size_t vallen;
+    if(0!=nn_getsockopt(s, NN_SOL_SOCKET, NN_RCVFD, &nn_poll_fd, &vallen)) {
+        throw std::string("nn_setsockopt(NN_LINGER)");
+    }
 
-	ev.data.fd = nn_poll_fd;
-	if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, nn_poll_fd, &ev) == -1)
-		throw std::string("epoll_ctl call for nn_poll_fd failed");
+    ev.data.fd = nn_poll_fd;
+    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, nn_poll_fd, &ev) == -1)
+        throw std::string("epoll_ctl call for nn_poll_fd failed");
 
-	bool binded = false;
-	if(cfg.bind_urls.empty()){
-		throw std::string("no listen endpoints specified. check your config");
-	}
+    bool binded = false;
+    if(cfg.bind_urls.empty()){
+        throw std::string("no listen endpoints specified. check your config");
+    }
 
-	for(const auto &i : cfg.bind_urls) {
-		const char *url = i.c_str();
-		ret = nn_bind(s, url);
-		if(ret < 0){
-			err("can't bind to url '%s': %d (%s)",url,
-				 errno,nn_strerror(errno));
-			continue;
-		}
-		binded = true;
-		info("listen on %s",url);
-	}
+    for(const auto &i : cfg.bind_urls) {
+        const char *url = i.c_str();
+        ret = nn_bind(s, url);
+        if(ret < 0){
+            err("can't bind to url '%s': %d (%s)",url,
+                errno,nn_strerror(errno));
+            continue;
+        }
+        binded = true;
+        info("listen on %s",url);
+    }
 
-	if(!binded){
-		err("there are no listened interfaces after bind. check log and your config");
-		throw std::string("can't bind to any url");
-	}
+    if(!binded){
+        err("there are no listened interfaces after bind. check log and your config");
+        throw std::string("can't bind to any url");
+    }
 
-	pthread_setname_np(pthread_self(), "dispatcher");
+    pthread_setname_np(pthread_self(), "dispatcher");
 
-	bool _stop = false;
-	while(!_stop) {
-		ret = epoll_wait(epoll_fd,events,EPOLL_MAX_EVENTS,-1);
-		if(ret == -1 && errno != EINTR){
-			err("dispatcher: epoll_wait: %s",strerror(errno));
-		}
-		if(ret < 1)
-			continue;
+    bool _stop = false;
+    while(!_stop) {
+        ret = epoll_wait(epoll_fd,events,EPOLL_MAX_EVENTS,-1);
+        if(ret == -1 && errno != EINTR){
+            err("dispatcher: epoll_wait: %s",strerror(errno));
+        }
+        if(ret < 1)
+            continue;
 
-		for (int n = 0; n < ret; ++n) {
-			struct epoll_event &e = events[n];
-			if(!(e.events & EPOLLIN)){
-				continue;
-			}
-			if(e.data.fd == stop_event_fd) {
-				dbg("got stop event. terminate");
-				_stop = true;
-				break;
-			}
+        for (int n = 0; n < ret; ++n) {
+            struct epoll_event &e = events[n];
+            if(!(e.events & EPOLLIN)){
+                continue;
+            }
+            if(e.data.fd == stop_event_fd) {
+                dbg("got stop event. terminate");
+                _stop = true;
+                break;
+            }
 
-			char *msg = nullptr;
-			int l = nn_recv(s, &msg, NN_MSG, 0);
-			if(l < 0) {
-				if(errno==EINTR) continue;
-				if(errno==EBADF) {
-					err("nn_recv returned EBADF. terminate");
-					_stop = true;
-					break;
-				}
-				dbg("nn_recv() = %d, errno = %d(%s)",l,errno,nn_strerror(errno));
-				//!TODO: handle timeout, etc
-				continue;
-			}
-			process_peer(msg,l);
-			nn_freemsg(msg);
-		}
-	}
-	nn_close(s);
+            char *msg = nullptr;
+            int l = nn_recv(s, &msg, NN_MSG, 0);
+            if(l < 0) {
+                if(errno==EINTR) continue;
+                if(errno==EBADF) {
+                    err("nn_recv returned EBADF. terminate");
+                    _stop = true;
+                    break;
+                }
+                dbg("nn_recv() = %d, errno = %d(%s)",l,errno,nn_strerror(errno));
+                //!TODO: handle timeout, etc
+                continue;
+            }
+            process_peer(msg,l);
+            nn_freemsg(msg);
+        }
+    }
+    nn_close(s);
 }
 
 void _dispatcher::stop()
 {
-	eventfd_write(stop_event_fd, 1);
+    eventfd_write(stop_event_fd, 1);
 }
 
 int _dispatcher::process_peer(char *msg, int len)
 {
-	char *reply = NULL;
-	int reply_len = 0;
+    char *reply = nullptr;
 
-	try {
-		create_reply(reply,reply_len,msg,len);
-  } catch(const std::string & e){
-		err("got string exception: %s",e.c_str());
-    create_error_reply(reply,reply_len,ECErrorId::GENERAL_ERROR,std::string("Internal Error"));
-  } catch(const CResolverError & e){
-    err("got resolve exception: <%u> %s", static_cast<uint>(e.code()), e.what());
-    create_error_reply(reply,reply_len,e.code(),e.what());
-	}
+    try {
+        reply = create_reply(msg,len);
+    } catch(const std::string & e){
+        err("got string exception: %s",e.c_str());
+        reply = create_error_reply(ECErrorId::GENERAL_ERROR,e);
+    } catch(const CResolverError & e){
+        err("got resolve exception: <%u> %s", static_cast<uint>(e.code()), e.what());
+        reply = create_error_reply(e.code(),e.what());
+    }
 
-	int l = nn_send(s, reply, reply_len, 0);
-	delete[] reply;
-	if(l!=reply_len){
-		err("nn_send(): %d, while msg to send size was %d. errno = %d(%s)",l,reply_len, errno,nn_strerror(errno));
-	}
-	return 0;
+    if(-1==nn_send(s, &reply, NN_MSG, 0)) {
+        err("nn_send(): %d(%s)",errno,nn_strerror(errno));
+    }
+
+    return 0;
 }
 
-void _dispatcher::str2reply(char *&msg,int &len,const ECErrorId code, const std::string &s)
+char *_dispatcher::str2reply(const ECErrorId code, const std::string &s)
 {
-	int l = s.size();
-	len = l+PDU_HDR_SIZE;
-	msg = new char[len];
-  msg[0]  = static_cast<char> (code);
-	msg[1] = l;
-	memcpy(msg+PDU_HDR_SIZE,s.c_str(),l);
+    auto l = s.size();
+    auto msg = static_cast<char *>(nn_allocmsg(l+PDU_HDR_SIZE, 0));
+
+    msg[0]  = static_cast<char>(code);
+    msg[1] = static_cast<char>(l);
+
+    memcpy(msg+PDU_HDR_SIZE,s.c_str(),l);
+
+    return msg;
 }
 
-void _dispatcher::make_reply(char *&msg,int &len,const CDriver::SResult_t &r)
+char *_dispatcher::make_reply(const CDriver::SResult_t &r)
 {
-    int lrn_len = r.localRoutingNumber.size();
-    int tag_len = r.localRoutingTag.size();
-    int data_len = lrn_len + tag_len;
+    auto lrn_len = r.localRoutingNumber.size();
+    auto tag_len = r.localRoutingTag.size();
+    auto data_len = lrn_len + tag_len;
 
-    len = data_len+NEW_PDU_HDR_SIZE;
-    msg = new char[len];
+    auto msg = static_cast<char *>(nn_allocmsg(data_len+NEW_PDU_HDR_SIZE, 0));
+
     msg[0] = static_cast<char> (ECErrorId::NO_ERROR); //code
-    msg[1] = data_len;  //global_len
-    msg[2] = lrn_len;   //lrn_len
+    msg[1] = static_cast<char>(data_len);  //global_len
+    msg[2] = static_cast<char>(lrn_len);   //lrn_len
 
     memcpy(msg+NEW_PDU_HDR_SIZE,r.localRoutingNumber.c_str(),lrn_len);
     memcpy(msg+NEW_PDU_HDR_SIZE+lrn_len,r.localRoutingTag.c_str(),tag_len);
+
+    return msg;
 }
 
-void _dispatcher::create_error_reply(char *&msg, int &len, const ECErrorId code, const std::string description)
+char *_dispatcher::create_error_reply(const ECErrorId code, const std::string description)
 {
-  str2reply(msg,len,code,description);
+   return str2reply(code,description);
 }
 
-void _dispatcher::create_reply(char *&msg, int &len, const char *req, int req_len)
+char *_dispatcher::create_reply(const char *req, int req_len)
 {
-    if(req_len < PDU_HDR_SIZE){
+    if(req_len < PDU_HDR_SIZE) {
         throw CResolverError(ECErrorId::PSQL_INVALID_REQUEST, "request too small");
     }
 
@@ -184,7 +187,7 @@ void _dispatcher::create_reply(char *&msg, int &len, const char *req, int req_le
     int lnp_offset = PDU_HDR_SIZE;
     bool old_format = data_len!=0;
 
-    if(!old_format){
+    if(!old_format) {
         if(req_len < NEW_PDU_HDR_SIZE){
             throw CResolverError(ECErrorId::PSQL_INVALID_REQUEST, "request too small");
         }
@@ -195,20 +198,20 @@ void _dispatcher::create_reply(char *&msg, int &len, const char *req, int req_le
 
     (void) version; // compiling error suppression
 
-    if((lnp_offset+data_len) > req_len){
+    if((lnp_offset+data_len) > req_len) {
         err("malformed request: too big data_len");
         throw CResolverError(ECErrorId::PSQL_INVALID_REQUEST, "malformed request");
     }
 
-    std::string lnp(req+lnp_offset,data_len);
+    std::string lnp(req+lnp_offset,static_cast<size_t>(data_len));
     dbg("process request: database: %d, lnp: %s, old_format: %d",
         database_id,lnp.c_str(),old_format);
 
     resolver::instance()->resolve(database_id,lnp,r);
 
-    if(old_format){
-        str2reply(msg,len,ECErrorId::NO_ERROR,r.localRoutingNumber);
+    if(old_format) {
+        return str2reply(ECErrorId::NO_ERROR,r.localRoutingNumber);
     } else {
-        make_reply(msg,len,r);
+        return make_reply(r);
     }
 }
--- server/src/dispatcher.h
+++ server/src/dispatcher.h
@@ -16,22 +16,24 @@
 #include "singleton.h"
 
 class _dispatcher {
-	int s;
-	int stop_event_fd;
+    int s;
+    int stop_event_fd;
 
-	int process_peer(char *msg, int len);
-  void str2reply(char *&msg,int &len,const ECErrorId code,const std::string &s);
-	void make_reply(char *&msg,int &len,const CDriver::SResult_t &r);
-	void create_reply(char *&msg, int &len, const char *req, int req_len);
-  void create_error_reply(char *&msg, int &len,const ECErrorId code,const std::string description);
+    int process_peer(char *msg, int len);
+
+    char *str2reply(const ECErrorId code,const std::string &s);
+    char *make_reply(const CDriver::SResult_t &r);
+    char *create_reply(const char *req, int req_len);
+    char *create_error_reply(const ECErrorId code,const std::string description);
 
   protected:
-	void dispose() {}
-  public:
-	_dispatcher();
+    void dispose() {}
 
-	void loop();
-	void stop();
+  public:
+    _dispatcher();
+
+    void loop();
+    void stop();
 };
 
 typedef singleton<_dispatcher> dispatcher;
